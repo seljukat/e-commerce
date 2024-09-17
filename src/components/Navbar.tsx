0dcc2dc50667536d4af02b8fb29d44337ce6259e
Selçuk Topkaya <seljukat@gmail.com>
2024-09-17 22:03:07 +0300

    Implement Footer on Homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
-          <div className="text-2xl tracking-wide">Storey</div>
+          <div className="text-2xl tracking-wide">STOREY</div>
         </Link>
         <Menu />
       </div>
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="" width={24} height={24} />
-            <div className="text-2xl tracking-wide">Storey</div>
+            <div className="text-2xl tracking-wide">STOREY</div>
           </Link>
           <div className="hidden xl:flex gap-4">
             <Link href="/">Homepage</Link>
