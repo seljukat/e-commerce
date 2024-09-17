685f9fe8c6fd8a3fb356c79373e4f029e7ee4ca4
Selçuk Topkaya <seljukat@gmail.com>
2024-09-17 18:43:43 +0300

    Implement Slider in Homepage

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Image from "next/image";
+
+const CartModal = () => {
+  const cartItems = true;
+
+  return (
+    <div className="w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20">
+      {!cartItems ? (
+        <div>Cart is empty</div>
+      ) : (
+        <>
+          <h2 className="text-xl">Shopping Cart</h2>
+          {/* LIST */}
+          <div className="flex flex-col gap-8">
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/14475824/pexels-photo-14475824.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div>
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="text-sm text-gray-500">available</div>
+                </div>
+
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+            {/* ITEM */}
+            <div className="flex gap-4">
+              <Image
+                src="https://images.pexels.com/photos/14475824/pexels-photo-14475824.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                alt=""
+                width={72}
+                height={96}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col justify-between w-full">
+                {/* TOP */}
+                <div>
+                  {/* TITLE */}
+                  <div className="flex items-center justify-between gap-8">
+                    <h3 className="font-semibold">Product Name</h3>
+                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                  </div>
+                  {/* DESC */}
+                  <div className="text-sm text-gray-500">available</div>
+                </div>
+
+                {/* BOTTOM */}
+                <div className="flex justify-between text-sm">
+                  <span className="text-gray-500">Qty. 2</span>
+                  <span className="text-blue-500">Remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* BOTTOM  */}
+          <div>
+            <div className="flex items-center justify-between font-semibold">
+              <span className="">Subtotal</span>
+              <span className="">$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Shipping and taxes calculated at checkout.
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
+                View cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CartModal;
