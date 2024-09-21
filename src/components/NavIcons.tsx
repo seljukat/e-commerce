57c675d4b229ef0fb16775884a9a12cdff51df23
Selçuk Topkaya <seljukat@gmail.com>
2024-09-21 19:07:09 +0300

    Implement authentications with Wix Headless

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -2,24 +2,59 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import CartModal from "./CartModal";
+import { useWixClient } from "@/hooks/useWixClient";
+import Cookies from "js-cookie";
 
 const NavIcons = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
+  // const pathName = usePathname();
 
   //   TEMPORARY
-  const isLoggedIn = false;
+  // const isLoggedIn = false;
+
+  const wixClient = useWixClient();
+
+  const isLoggedIn = wixClient.auth.loggedIn();
 
   const handleProfile = () => {
     if (!isLoggedIn) {
       router.push("/login");
+    } else {
+      setIsProfileOpen((prev) => !prev);
     }
-    setIsProfileOpen((prev) => !prev);
+  };
+
+  // AUTH WITH WIX-MANAGED AUTH
+  // const wixClient = useWixClient();
+
+  // const login = async () => {
+  //   const loginRequestData = wixClient.auth.generateOAuthData(
+  //     "http://localhost:3000"
+  //     // "https://www.mysite.com/login",
+  //   );
+  //   console.log(loginRequestData);
+
+  //   localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
+
+  //   const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
+
+  //   window.location.href = authUrl;
+  // };
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    Cookies.remove("refreshToken");
+    const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+    setIsLoading(false);
+    setIsProfileOpen(false);
+    router.push(logoutUrl);
   };
 
   return (
@@ -31,11 +66,14 @@ const NavIcons = () => {
         height={22}
         className="cursor-pointer"
         onClick={handleProfile}
+        // onClick={login}
       />
       {isProfileOpen && (
-        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+        <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/">Profile</Link>
-          <div className="mt-2 cursor-pointer">Logout</div>
+          <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+            {isLoading ? "Logging out..." : "Logout"}
+          </div>
         </div>
       )}
       <Image
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
 import SearchBar from "./SearchBar";
-import NavIcons from "./NavIcons";
+import dynamic from "next/dynamic";
+// import NavIcons from "./NavIcons";
+
+const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
 const Navbar = () => {
   return (
